cf0ae29b2cfa9342b85b18710114670205a40aee
Shubham <shubham.b.interns@aux.gojek.com>
2021-09-04 15:58:37 +0530

    UPDATE: latest updates for internship and skills

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,133 +1,213 @@
 import React, { Component } from 'react';
 
 class Experience extends Component {
-    render() {
-        return (
-            <div>
-                <section className="colorlib-experience" data-section="experience">
-				<div className="colorlib-narrow-content">
-					<div className="row">
-						<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-							<span className="heading-meta">Experience</span>
-							<h2 className="colorlib-heading animate-box">Work Experience</h2>
+	render() {
+		return (
+			<div>
+				<section className="colorlib-experience" data-section="experience">
+					<div className="colorlib-narrow-content">
+						<div className="row">
+							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+								<span className="heading-meta">Experience</span>
+								<h2 className="colorlib-heading animate-box">Work Experience</h2>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-md-12">
+								<div className="timeline-centered">
+									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+										<div className="timeline-entry-inner">
+
+											<div className="timeline-icon color-1">
+												<i className="icon-pen2"></i>
+											</div>
+
+											<div className="timeline-label">
+												<h2><a href="https://www.gojek.io/" target="_blank">Gojek Bengaluru | Product Engineering Intern</a> <span>June 2021 - Present</span></h2>
+												<ul>
+													<li><p>Working with Gojek’s Customer Platform Backend team to build Customer Loyalty Programme.
+										</p></li>
+													<li>
+														<b>Tools and Technology used</b>:- Go,Ruby,Chef,Aerospike DB,Spanner DB,A/B testing Platform.
+													</li>
+												</ul>
+
+
+
+
+											</div>
+										</div>
+									</article>
+									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+										<div className="timeline-entry-inner">
+
+											<div className="timeline-icon color-1">
+												<i className="icon-pen2"></i>
+											</div>
+
+											<div className="timeline-label">
+												<h2><a href="https://esamudaay.com/about/" target="_blank">eSamudaay Bengaluru | SDE Intern</a> <span>March 2021 - June 2021</span></h2>
+												<ul>
+													<li>
+														Built Probability based Payment System and Messaging ser-
+														vice which reduces chances for single point of failures and run
+														even in case of third party failure.
+													</li>
+													<li>
+														Built Dashboard on ReactJs with Hooks for Business Analysts to get the Order data for company based on various filters.
+													</li>
+													<li>
+														Created APIs based on need , resolved various Bugs, and built automated services for admin in existing code.
+													</li>
+													<li>
+														<p><b>Tools and Technology used</b>:- Python,React,Django,Django Rest Framework,Postgres,Redis,Redis-Search.</p>
+													</li>
+												</ul>
+											</div>
+										</div>
+									</article>
+									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+										<div className="timeline-entry-inner">
+
+											<div className="timeline-icon color-1">
+												<i className="icon-pen2"></i>
+											</div>
+
+											<div className="timeline-label">
+												<h2><a href="https://mewt.in/" target="_blank">MEWT Bengaluru | Backend Engineering Intern</a> <span>Jan 2021 - March-2021</span></h2>
+												<ul>
+													<li>
+														Automated the process to fetch Account details using Django framework,which helped to reduce truckloads of redundant work.
+													</li>
+													<li>
+														Successfully built and tested APIs,to store fetched data in database with authentication and authorization.
+													</li>
+													<li>
+														To reduce the latency in the time-consuming task, used the asynchronous way and modified the task using celery and RabbitMQ.
+													</li>
+													<li>
+														<p><b>Tools and Technology used</b>:- Python,Django Rest Framework,MySQL,Redis,Celery</p>
+													</li>
+												</ul>
+
+											</div>
+										</div>
+									</article>
+									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+										<div className="timeline-entry-inner">
+											<div className="timeline-icon color-2">
+												<i className="icon-pen2"></i>
+											</div>
+											<div className="timeline-label">
+												<h2><a >Hotel Management System</a> <span>Jan 2019</span></h2>
+												<ul>
+													<li>
+														Developed a web-app for Hostel/Hotel Management System
+														which gives all the functionality from Room Booking,Food
+														Ordering and Room Availability status.
+													</li>
+													<li>
+														Implemented other backend features such as validation and
+														login authentication,Scheduled tasks.
+													</li>
+													<li>
+														<b>Tools and Technology used</b>:- Django,Python,Ajax,Celery,RabbitMQ,MySQL.
+													</li>
+
+												</ul>
+
+											</div>
+										</div>
+									</article>
+									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+										<div className="timeline-entry-inner">
+
+											<div className="timeline-icon color-2">
+												<i className="icon-pen2"></i>
+											</div>
+
+											<div className="timeline-label">
+												<h2><a href="https://github.com/Shubham-bhawsar/FarmZop">FarmZop | B.tech Project</a> <span>Aug 2020 - Nov 2020</span></h2>
+												<ul>
+													<li>
+														Developed a web-app on Flask framework to help the farmers in predicting Early-Yield and Disease Detection.
+													</li>
+													<li>
+														Built the Machine Learning Based Model for Crop Yield Prediction and Deep Learning based model for Disease Detection.
+													</li>
+													<li>
+														Deployed the entire model through flask.
+													</li>
+												</ul>
+
+											</div>
+										</div>
+									</article>
+
+
+									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+										<div className="timeline-entry-inner">
+
+											<div className="timeline-icon color-4">
+												<i className="icon-pen2"></i>
+											</div>
+
+											<div className="timeline-label">
+												<h2><a href="#colorlib-hero">Portfolio on React</a> <span>Nov 2020</span></h2>
+												<p>This portfolio is built on react and with backend services on Node.</p>
+											</div>
+										</div>
+									</article>
+
+
+									<article className="timeline-entry animate-box" data-animate-effect="fadeInRight">
+										<div className="timeline-entry-inner">
+											<div className="timeline-icon color-5">
+												<i className="icon-pen2"></i>
+											</div>
+											<div className="timeline-label">
+												<h2><a href="https://github.com/Shubham-bhawsar/AL-Task">AL-Task</a> <span>June 2019 -July 2019</span></h2>
+												<p>• Built a system which helps the analysts to fetch the data di-
+												rectly from database into google sheets to perform Data Anal-
+										ysis and further evaluation on data.</p><p> • Theory Of Computation
+												• Built API to connect Google Sheets and Database and sync
+										the data whenever required.</p>
+											</div>
+										</div>
+									</article>
+
+									<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+										<div className="timeline-entry-inner">
+											<div className="timeline-icon color-1">
+												<i className="icon-pen2"></i>
+											</div>
+											<div className="timeline-label">
+												<h2><a href="https://play.google.com/store/apps/details?id=com.infotsav.test&hl=en">Infotsav App</a> <span>Dec 2018 - Jan 2019</span></h2>
+												<p>This app was build for Central India biggest technical fest Infotsav 2k19 hosted by ABV-IIITM Gwalior</p>
+											</div>
+										</div>
+									</article>
+
+
+
+
+
+
+									<article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+										<div className="timeline-entry-inner">
+											<div className="timeline-icon color-none">
+											</div>
+										</div>
+									</article>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div className="row">
-						<div className="col-md-12">
-				         <div className="timeline-centered">
-						   <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-					            <div className="timeline-entry-inner">
+				</section>
 
-					               <div className="timeline-icon color-1">
-					                  <i className="icon-pen2"></i>
-					               </div>
-
-					               <div className="timeline-label">
-					                  <h2><a href="https://mewt.in/">MEWT Bengaluru | Backend Engineering Intern</a> <span>Jan 2021 - Present</span></h2>
-					                  <p>It's a startup at Bengaluru which provides loan facilities to MSMEs to ensure they don't gey stucked in deadlock of their payment lifecycle and grow more in the future.
-									 I am Currently working here as Backend Engineering Intern and Automated the process to fetch Account details using Django
-										framework,which helped to reduce truckloads of redundant
-										work.
-										</p>
-					               </div>
-					            </div>
-					        </article>
-							<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-					            <div className="timeline-entry-inner">
-
-					               <div className="timeline-icon color-2">
-					                  <i className="icon-pen2"></i>
-					               </div>
-
-					               <div className="timeline-label">
-					                  <h2><a href="https://github.com/Shubham-bhawsar/FarmZop">FarmZop | B.tech Project</a> <span>Aug 2020 - Nov 2020</span></h2>
-					                  <p>•Developed a web-app on Flask framework to help the farmers
-											in predicting Early-Yield and Disease Detection.</p>
-											<p>• Built the Machine Learning Based Model for Crop Yield Pre-
-											diction and Deep Learning based model for Disease Detection.</p>
-											<p>• Deployed the entire model through flask.</p>
-					               </div>
-					            </div>
-					         </article>
-						   
-
-					         <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-					            <div className="timeline-entry-inner">
-
-					               <div className="timeline-icon color-4">
-					                  <i className="icon-pen2"></i>
-					               </div>
-
-					               <div className="timeline-label">
-					                  <h2><a href="#colorlib-hero">Portfolio on React</a> <span>Nov 2020</span></h2>
-					                  <p>This portfolio is built on react and with backend services on Node.</p>
-					               </div>
-					            </div>
-					         </article>
-
-
-					         <article className="timeline-entry animate-box" data-animate-effect="fadeInRight">
-					            <div className="timeline-entry-inner">
-					               <div className="timeline-icon color-5">
-					                  <i className="icon-pen2"></i>
-					               </div>
-					               <div className="timeline-label">
-					               	<h2><a href="https://github.com/Shubham-bhawsar/AL-Task">AL-Task</a> <span>June 2019 -July 2019</span></h2>
-					                  <p>• Built a system which helps the analysts to fetch the data di-
-										rectly from database into google sheets to perform Data Anal-
-										ysis and further evaluation on data.</p><p> • Theory Of Computation
-										• Built API to connect Google Sheets and Database and sync
-										the data whenever required.</p>
-					               </div>
-					            </div>
-					         </article>
-
-					         <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-					            <div className="timeline-entry-inner">
-					               <div className="timeline-icon color-1">
-					                  <i className="icon-pen2"></i>
-					               </div>
-					               <div className="timeline-label">
-					               	<h2><a href="https://play.google.com/store/apps/details?id=com.infotsav.test&hl=en">Infotsav App</a> <span>Dec 2018 - Jan 2019</span></h2>
-					                  <p>This app was build for Central India biggest technical fest Infotsav 2k19 hosted by ABV-IIITM Gwalior</p>
-					               </div>
-					            </div>
-					         </article>
-							 <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-					            <div className="timeline-entry-inner">
-					               <div className="timeline-icon color-2">
-					                  <i className="icon-pen2"></i>
-					               </div>
-					               <div className="timeline-label">
-					               	<h2><a >Hotel Management System</a> <span>Jan 2019</span></h2>
-									   <p>• Developed a web-app for Hostel/Hotel Management System
-										which gives all the functionality from Room Booking,Food
-										Ordering and Room Availability status.</p>
-										<p>• Implemented other backend features such as validation and
-										login authentication,Scheduled tasks.</p>
-					               </div>
-					            </div>
-					         </article>
-
-					        
-
-			
-
-					         <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
-					            <div className="timeline-entry-inner">
-					               <div className="timeline-icon color-none">
-					               </div>
-					            </div>
-					         </article>
-					      </div>
-					   </div>
-				   </div>
-				</div>
-			</section>
-
-            </div>
-        );
-    }
+			</div>
+		);
+	}
 }
 
 export default Experience;
